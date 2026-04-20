3c813ab334b8a8444f7cf384767cc59b870aca84
Victor Touriño Fernandez <touri.victor22@gmail.com>
2026-04-20 09:44:39 +0200

    Update shared.js

--- shared.js
+++ shared.js
@@ -238,7 +238,7 @@ window.initSharedUI = function() {
         }
         var html = '';
         results.forEach(function(p) {
-            html += '<a href="/producto.html?id=' + p.id + '" class="search-result-item">' +
+            html += '<a href="producto.html?id=' + p.id + '" class="search-result-item">' +
                 '<img src="' + p.imagen + '" alt="' + p.nombre + '" class="search-result-img">' +
                 '<div class="search-result-info"><div class="search-result-name">' + p.nombre + '</div>' +
                 '<div class="search-result-cat">' + p.categoria + '</div></div>' +
