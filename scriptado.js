a2b153d948a15873db6abfcd5743342aaeeb012f
Victor Touriño Fernandez <touri.victor22@gmail.com>
2026-04-20 12:20:45 +0200

    Update scriptado.js

--- scriptado.js
+++ scriptado.js
@@ -442,4 +442,31 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
+    document.querySelectorAll('.carousel-container').forEach(carousel => {
+    const slider = carousel.querySelector('.products-grid');
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    slider.addEventListener('mousedown', (e) => {
+        isDown = true;
+        slider.style.cursor = 'grabbing';
+        startX = e.pageX - slider.offsetLeft;
+        scrollLeft = slider.scrollLeft;
+    });
+    slider.addEventListener('mouseleave', () => {
+        isDown = false;
+        slider.style.cursor = 'grab';
+    });
+    slider.addEventListener('mouseup', () => {
+        isDown = false;
+        slider.style.cursor = 'grab';
+    });
+    slider.addEventListener('mousemove', (e) => {
+        if(!isDown) return;
+        e.preventDefault();
+        const x = e.pageX - slider.offsetLeft;
+        const walk = (x - startX) * 2; // Velocidad de desplazamiento
+        slider.scrollLeft = scrollLeft - walk;
+    });
 });
